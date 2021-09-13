9232c9504ca1f1c66456ca707e648f0e5affe8a7
seaona <mariona@gmx.es>
2021-09-13 15:41:16 +0200

    FEATURE: added eth bit functionalities

---
+++ README.md
@@ -0,0 +1,3 @@
+https://npm.io/package/vanity-eth
+
+https://www.npmjs.com/package/vanity-btc
--- index.js
+++ index.js
@@ -5,6 +5,28 @@ const app = express()
 
 const PORT = process.env.PORT || 8000
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    res.render('pages/home');
+});
+
+app.post('/ethereum', async (req, res) =>{
+    const data = await generateVanityETH(req.body.ethereum)
+    res.render('pages/address', {adreça: data.address, clau_privada: data.privKey})
+});
+
+app.post('/bitcoin', async (req, res) =>{
+    const data = await generateVanityBTC(req.body.bitcoin)
+    res.render('pages/address', {adreça: data.address, clau_privada: data.privKey})
+});
+
 const generateVanityBTC = async (string) => {
     const wallet = await vanity_btc.getVanityWallet(string)
     return wallet
@@ -15,8 +37,8 @@ const generateVanityETH = async (string) => {
     return wallet
 }
 
-generateVanityBTC("99")
-generateVanityETH("99")
+//generateVanityBTC("99")
+//generateVanityETH("99")
 
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
